1ccf4ba8f001f06063aa0379a7f12a0d8bd67c6a
Fredy Yanardi <fyanardi@gmail.com>
2025-05-20 13:26:37 +0800

    initial version of Neovim LUA configuration

---
+++ .config/nvim/init.lua
@@ -0,0 +1,41 @@
+-- Basic configurations
+vim.g.mapleader = " "
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.list = true
+vim.o.clipboard = "unnamed"
+vim.o.number = true
+vim.o.termguicolors = true
+vim.g.onedark_terminal_italics = 2
+
+-- Nerd font
+-- 1. download from https://www.nerdfonts.com/
+-- 2. extract to `~/.fonts`
+-- 3. run `fc-cache -fv` to rebuild the font cache
+if vim.g.fvim_loaded then
+  vim.cmd("set guifont=FiraCode\\ NF:h14")
+end
+
+require("config.lazy")
+
+-- onedark colorschema (installed via lazy.vim)
+vim.cmd(":colorscheme onedark")
+
+-- Different tabstop for python and ts/tsx
+vim.cmd("autocmd BufEnter *.py :setlocal tabstop=4 shiftwidth=4 expandtab")
+vim.cmd("autocmd BufEnter *.js,*.ts,*.tsx :setlocal tabstop=2 shiftwidth=2 expandtab")
+
+-- ignore node_modules from grep search
+vim.cmd(":set wildignore=*/node_modules/*")
+
+-- NerdTree
+vim.cmd("nnoremap <C-n> :NvimTreeToggle<CR>")
+
+-- Telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,123 @@
+return {
+  -- show search results using folds
+  {
+    "vim-scripts/searchfold.vim",
+  },
+
+  -- nvim-rg allows you to run ripgrep from Neovim or Vim and shows the results
+  -- in a quickfix window. It was developed for use on macOS with Neovim. On
+  -- Neovim, it runs asynchronously.
+  {
+    "duane9/nvim-rg",
+  },
+
+  -- A File Explorer For Neovim Written In Lua
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {
+        sync_root_with_cwd = false,
+        respect_buf_cwd = false,
+        disable_netrw = false,
+        hijack_netrw = false,
+        update_focused_file = {
+          enable = true,
+        },
+        git = {
+          enable = false,
+        },
+      }
+    end,
+  },
+
+  -- telescope.nvim is a highly extendable fuzzy finder over lists
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup {
+        defaults = {
+          "^node_modules/",
+          "^.git/",
+        },
+        -- disable treesitter in preview
+        preview = {
+        },
+      }
+    end,
+  },
+
+  -- The goal of nvim-treesitter is both to provide a simple and easy way to
+  -- use the interface for tree-sitter in Neovim and to provide some basic
+  -- functionality such as highlighting based on it
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup {
+        ensure_installed = {
+          "yaml",
+          "lua",
+        },
+        ignore_install = { },
+        highlight = { enable = true },
+        indent = { enable = true },
+      }
+    end,
+  },
+
+  -- nvim-lspconfig is a "data only" repo, providing basic, default Nvim LSP
+  -- client configurations for various LSP servers.
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local nvim_lsp = require('lspconfig')
+      local servers = { 'pyright', 'ts_ls' }
+      for _, lsp in ipairs(servers) do
+        nvim_lsp[lsp].setup {
+          capabilities = capabilities,
+          -- on_attach = on_attach,
+          flags = {
+            debounce_text_changes = 150,
+          }
+        }
+      end
+    end,
+  },
+
+  -- A completion engine plugin for neovim written in Lua
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+    },
+  },
+
+  -- project.nvim is an all in one neovim plugin written in lua that provides
+  -- superior project management.
+  {
+    "ahmedkhalf/project.nvim",
+    config = function()
+      require("project_nvim").setup {
+        patterns = { ".git" },
+      }
+    end,
+  },
+
+  -- onedark.vim: A dark Vim/Neovim color scheme for the GUI and
+  -- 16/256/true-color terminals, based on FlatColor, with colors inspired by
+  -- the excellent One Dark syntax theme for the Atom text editor.
+  {
+    "joshdick/onedark.vim",
+  },
+}
