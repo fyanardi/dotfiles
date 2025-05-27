25a72b253b32d125bbf86297d531376292d7af60
Fredy Yanardi <fyanardi@gatech.edu>
2025-05-27 17:08:46 +0800

    enable pyright LSP

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -39,3 +39,9 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find f
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+
+-- LSP
+-- Nvim 0.10
+require('lspconfig').pyright.setup{}
+-- Nvim 0.11+
+-- vim.lsp.enable('pyright')
