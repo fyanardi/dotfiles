8c2fbec90b5d894d6beae3a2f20d718c5979621d
Fredy Yanardi <fyanardi@gmail.com>
2025-07-19 12:08:07 +0800

    add conjure Neovim plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -184,4 +184,31 @@ return {
     'rcarriga/nvim-dap-ui',
     dependencies = 'mfussenegger/nvim-dap',
   },
+
+  {
+    "Olical/conjure",
+    ft = { "clojure" },
+    lazy = false,
+    init = function()
+      -- Set configuration options here
+      -- Uncomment this to get verbose logging to help diagnose internal Conjure issues
+      -- This is VERY helpful when reporting an issue with the project
+      -- vim.g["conjure#debug"] = true
+    end,
+
+    -- Optional cmp-conjure integration
+    dependencies = { "PaterJason/cmp-conjure" },
+  },
+
+  {
+    "PaterJason/cmp-conjure",
+    lazy = false,
+    config = function()
+      local cmp = require("cmp")
+      local config = cmp.get_config()
+      table.insert(config.sources, { name = "conjure" })
+      return cmp.setup(config)
+    end,
+  },
+
 }
